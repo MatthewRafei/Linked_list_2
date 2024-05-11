87f20b695af1cb549276b6adb55249654fb480fc
oddstap <matthewrafei1@gmail.com>
2024-05-10 22:18:44 -0500

    Working on it

--- linked_list.c
+++ linked_list.c
@@ -5,6 +5,7 @@
 
 #include "linked_list.h"
 
+// Compound statement
 LinkedList llcreate(void) {
     return (LinkedList) {
         .head = NULL,
@@ -18,54 +19,99 @@ Node *node_alloc(int value) {
     return n;
 }
 
-// 1 2 3 4
-//       h
+void *node_free(Node *n) {
+    free(n);
+}
 
-// NULL <- 1 <- 2 <- 3 <- 4 <- 5
-//                             h
+// 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+// h
 
 void llappend(LinkedList *ll, int data) {
-    Node *n = malloc(sizeof(Node));
-    n->data = data;
+    //Node *n = malloc(sizeof(Node));
+    //n->data = data;
+    //n->next = ll->head;
+    //ll->head = n;
+
+    Node *n = node_alloc(data);
     n->next = ll->head;
     ll->head = n;
+
 }
 
 void llremove_at(LinkedList *ll, size_t idx) {
-    assert(0 && "unimplemented");
+    Node *n = ll->head;
+    Node *p = NULL;
+    for(int i = 0; i < idx; i++){
+        n = n -> next;
+    }
+
+    printf("What is the data %d at node %d", n->data, idx - 1);
 }
 
+// remove node with element equal to argument
+// We dont need an edge case for head because next equals previous node
 void llremove(LinkedList *ll, int elem) {
-    assert(0 && "unimplemented");
+    Node *n = ll->head;
+    Node *p = NULL;
+
+    while(n->next->data != elem){
+        n = n->next;
+    }
+
+    p = n->next;
+    n->next = n->next->next;
+
+    // Am I freeing this correctly?
+    node_free(p);
 }
 
 void llremove_all(LinkedList *ll, int elem) {
     assert(0 && "unimplemented");
 }
 
+// Why is it zero indexed?
 int llget_at(LinkedList *ll, size_t idx) {
-    assert(0 && "unimplemented");
+    Node *n = ll->head;
+    for(int i = 0; i < idx; i++){
+        n = n->next;
+    }
+
+    return n->data;
 }
 
 void llprint(LinkedList *ll) {
     Node *n = ll->head;
 
-    while(n != NULL){
+    while(n){
         printf("%d", n->data);
 
+        // Because n->next will equal null which is false
         if (n->next) {
-            printf(" <- ");
+            printf(" -> ");
+        }
+        else{
+            printf(" -> NULL");
         }
 
         n = n->next;
     }
 }
 
+// Get length
 size_t llget_length(LinkedList *ll) {
-    assert(0 && "unimplemented");
+    Node *n = ll->head;
+    size_t count = 0;
+    while(n){
+        count++;
+        n = n->next;
+    }
+
+    return count;
 }
 
+// This is destroying the linked list
 void llfree(LinkedList *ll) {
+    assert(0 && "unimplemented");
 }
 
 /*void good_function(void) {
--- main.c
+++ main.c
@@ -10,8 +10,26 @@ int main(void) {
     llappend(&ll, 3);
     llappend(&ll, 4);
     llappend(&ll, 5);
+    llappend(&ll, 6);
+    llappend(&ll, 7);
 
     llprint(&ll);
+
+    printf("\n");
+
+    printf("The length of the linked list %d\n", llget_length(&ll));
+
+    int index = 0;
+
+    printf("Value at index %d in Linked list is %d\n", index, llget_at(&ll, index));
+
+    printf("The head is %d\n", ll.head->data);
+
+    llremove(&ll, 5);
+
+    llprint(&ll);
+
+    llremove(&ll, 0);
 
     //good_function();
 
